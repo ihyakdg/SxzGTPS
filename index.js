f8e86149660368a025e3838b6d08ca62f3ed4026
ihyakdg <82592132+ihyakdg@users.noreply.github.com>
2026-08-18 15:43:37 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const serverData = `server|168.144.247.19
+port|55000
+type|1
+type2|1
+loginurl|nopy-gtps.vercel.app
+beta_server|127.0.0.1
+beta_port|17091
+beta_type|1
+meta|localhost
+RTENDMARKERBS1001`;
+
+app.post("/growtopia/server_data.php", (req, res) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send(serverData);
+});
+
+app.get("/growtopia/server_data.php", (req, res) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send(serverData);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running..."));
+
+module.exports = app;
